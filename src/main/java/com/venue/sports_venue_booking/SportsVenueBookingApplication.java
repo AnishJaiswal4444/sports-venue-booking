ced75e119f64ae3ec63ab1e194a419c6cb1ade14
AnishJaiswal4444 <anishjaiswal381@gmail.com>
2026-01-06 22:55:00 +0530

    Inital Comment :: setting up the project

---
+++ src/main/java/com/venue/sports_venue_booking/SportsVenueBookingApplication.java
@@ -0,0 +1,13 @@
+package com.venue.sports_venue_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsVenueBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsVenueBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venue/sports_venue_booking/SportsVenueBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venue.sports_venue_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsVenueBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
